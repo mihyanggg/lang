3e242303a0e0ad96fb17bfea5ee9fa10bf825ac2
mimi <mihyanggg@kakao.com>
2023-12-18 00:25:13 +0900

    modified ls.c

--- c/ls.c
+++ c/ls.c
@@ -1,31 +1,44 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 #include <dirent.h>
 
 int main(int argc, char* argv[]) {
     
     struct dirent *dir;
+    int c;
 
     DIR *dp = opendir(".");
     if (dp == NULL) {
         return -1;
     }
-    
-    while (dir = readdir(dp)) {
-        if (argc == 1) {
+
+    if (argc == 1) {
+        while (dir = readdir(dp)) {
             if (dir->d_name[0] == '.')
                 continue;
             else
                 printf("%s  ", dir->d_name);
         }
-        else if (strchr(argv[1], 'a')) {
-            printf("%s  ", dir->d_name);
+    closedir(dp);
+    }
+    else {
+        while( (c = getopt(argc, argv, ":abc:d")) != -1) {
+            switch(c) {
+                case 'a':
+                    while (dir = readdir(dp)) {
+                        printf("%s  ", dir->d_name);
+                    }
+                    break;
+                case ':':
+                    break;
+                case '?':
+                    printf("Does not support %c\n", optopt);
+            }
+            closedir(dp);
         }
     }
-
     printf("\n");
 
-    closedir(dp);
-    
     return 0;
 }
